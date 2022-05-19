5af8bc4a6cd752721a67d107245e854c9bcf5990
manitbaser <manitb06@gmail.com>
2022-05-19 12:14:54 +0530

    First Commit

---
+++ src/components/icons/icon.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  IconAppStore,
+  IconBookmark,
+  IconMedium,
+  IconExternal,
+  IconFolder,
+  IconFork,
+  IconGitHub,
+  IconFacebook,
+  IconLinkedin,
+  IconLoader,
+  IconLogo,
+  IconPlayStore,
+  IconStar,
+  IconTwitter,
+} from '@components/icons';
+
+const Icon = ({ name }) => {
+  switch (name) {
+    case 'AppStore':
+      return <IconAppStore />;
+    case 'Bookmark':
+      return <IconBookmark />;
+    case 'Medium':
+      return <IconMedium />;
+    case 'External':
+      return <IconExternal />;
+    case 'Folder':
+      return <IconFolder />;
+    case 'Fork':
+      return <IconFork />;
+    case 'GitHub':
+      return <IconGitHub />;
+    case 'Facebook':
+      return <IconFacebook />;
+    case 'Linkedin':
+      return <IconLinkedin />;
+    case 'Loader':
+      return <IconLoader />;
+    case 'Logo':
+      return <IconLogo />;
+    case 'PlayStore':
+      return <IconPlayStore />;
+    case 'Star':
+      return <IconStar />;
+    case 'Twitter':
+      return <IconTwitter />;
+    default:
+      return <IconExternal />;
+  }
+};
+
+Icon.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Icon;
---
+++ src/components/sections/about.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      background: transparent;
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+        box-shadow: 20px 20px 100px rgba(0, 255, 255, 0.4);
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              Hello! My name is Manit. I graduated from{' '}
+              <a
+                href="https://www.linkedin.com/school/birla-institute-of-technology-and-science-pilani/"
+                target="_blank"
+                rel="noreferrer">
+                Birla Institute of Technology and Science, Pilani, India
+              </a>
+              , where I pursued B.E. in Electrical and Electronics. I like solving real-world
+              problems through the applications of Deep Learning, Computer Vision and Software
+              Development.
+            </p>
+            <p>
+              Fast-forward to today, and I’ve had the privilege of working at{' '}
+              <a href="https://www.microsoft.com/">a pretty huge corporation</a>,{' '}
+              <a href="https://www.flipkart.com/">an E-Commerce marketplace</a>,{' '}
+              <a href="https://draup.com/">an AI-Driven enterprise start-up</a>, and{' '}
+              <a href="https://www.sunmobility.co.in/">an EV-based start-up</a>.
+            </p>
+            <p>Outside of work, I enjoy football, reading and speed-cubing.</p>
+          </div>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/me.jpg"
+              width={500}
+              quality={95}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
